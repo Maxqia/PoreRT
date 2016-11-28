742a0a50867a8205e3c94e3b495230cbe01d530c
Max Qian <public@maxqia.com>
2016-11-27 16:45:14 -0800

    Check for CustomInventory first and better method for updateInventory

--- src/main/java/blue/lapis/pore/impl/entity/PorePlayer.java
+++ src/main/java/blue/lapis/pore/impl/entity/PorePlayer.java
@@ -350,7 +350,7 @@ public class PorePlayer extends PoreHumanEntity implements org.bukkit.entity.Pla
     @Override
     public void updateInventory() {
         EntityPlayerMP player = ((EntityPlayerMP) getHandle());
-        player.sendContainerToPlayer(player.inventoryContainer);
+        player.inventoryContainer.detectAndSendChanges();
     } // No function to do this natively in Sponge
 
     @Override
